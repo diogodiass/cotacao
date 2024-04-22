839b2b3f8c870bc308724aa854ed2d6a1d4dfc71
Diogo Dias <diogo.amorim.dias@hotmail.com>
2024-04-22 19:59:56 -0300

    Create RegisterClient CurrencyPriceClient

---
+++ src/main/java/org/br/mineracao/repository/QuotationRepository.java
@@ -0,0 +1,10 @@
+package org.br.mineracao.repository;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.br.mineracao.entity.QuotationEntity;
+
+@ApplicationScoped
+public class QuotationRepository implements PanacheRepository<QuotationEntity> {
+}
